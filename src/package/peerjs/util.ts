97b77d1fa76d7d651fec4fe6e91d65e919df8e17
kolserdav <serega12101983@gmail.com>
2022-05-31 05:46:41 +0700

    Without turns

--- src/package/peerjs/util.ts
+++ src/package/peerjs/util.ts
@@ -14,14 +14,7 @@ export interface UtilSupportsObj {
 }
 
 const DEFAULT_CONFIG = {
-	iceServers: [{ urls: "stun:stun.l.google.com:19302" }, {
-		urls: [
-			"turn:eu-0.turn.peerjs.com:3478",
-			"turn:us-0.turn.peerjs.com:3478",
-		],
-		username: "peerjs",
-		credential: "peerjsp",
-	},],
+	iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 	sdpSemantics: "unified-plan",
 };
 
